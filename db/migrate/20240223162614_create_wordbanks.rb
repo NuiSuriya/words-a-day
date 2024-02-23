4fd6aa793eaa0094d47104dbe6ee2bbe98e88cb2
Nui Suriya <atchara.suriya@gmail.com>
2024-02-23 17:26:53 +0100

    create model wordbank

---
+++ db/migrate/20240223162614_create_wordbanks.rb
@@ -0,0 +1,9 @@
+class CreateWordbanks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :wordbanks do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_23_134423) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_23_162614) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_23_134423) do
     t.string "word"
     t.string "part_of_speech"
     t.text "definition"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "wordbanks", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
