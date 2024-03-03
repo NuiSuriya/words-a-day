bc0146252595d8734a1e79530a785e18e1e297bb
Nui Suriya <atchara.suriya@gmail.com>
2024-03-03 13:21:56 +0100

    add hammer.js

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -1,32 +1,35 @@
 import { Controller } from "@hotwired/stimulus"
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+import Hammer from "hammerjs";
 
 export default class extends Controller {
   connect() {
-    this.swiper = new Swiper(this.element, {
-      effect: 'cards',
-      on: {
-        slideChange: () => {
-          const direction = this.swiper.activeIndex > this.swiper.previousIndex ? 'right' : 'left';
-          const studied = direction === 'left';
-          const vocabId = this.swiper.slides[this.swiper.activeIndex].dataset.vocabId
-          // console.log(vocabId);
+    // SWIPER.js
+    // this.swiper = new Swiper(this.element, {
+    //   effect: 'cards',
+    //   on: {
+    //     slideChange: () => {
+    //       const direction = this.swiper.activeIndex > this.swiper.previousIndex ? 'right' : 'left';
+    //       const studied = direction === 'left';
+    //       const vocabId = this.swiper.slides[this.swiper.activeIndex].dataset.vocabId
+    //       if (vocabId) {
+    //         fetch(`/flashcard_words/${vocabId}`, {
+    //           method: 'PATCH',
+    //           headers: {
+    //             'Content-Type': 'application/json',
+    //             'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+    //           },
+    //             body: JSON.stringify({ studied: studied })
+    //         })
+    //         // .then(
+    //         //   console.log(this.swiper.slides[this.swiper.activeIndex].firstElementChild)
+    //         // )
+    //       }
+    //     },
+    //   },
+    // });
 
-          fetch(`/flashcard_words/${vocabId}`, {
-            method: 'PATCH',
-            headers: {
-              'Content-Type': 'application/json',
-              'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-            },
-              body: JSON.stringify({ studied: studied })
-          })
-          .then(() => {
-            // this.swiper.slides[this.swiper.activeIndex]
-            // this.swiper.removeSlide(this.swiper.activeIndex);
-          })
-
-        },
-      },
-    });
+    // Hammer.js
+    console.log(this.element);
   }
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "swiper/element/bundle", to: "swiper--element--bundle.js" # @11.0.6
+pin "hammerjs" # @2.0.8
