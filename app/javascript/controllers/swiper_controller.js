8eba95853a115ca02724fd38e4cc58582bc56f4a
Nui Suriya <atchara.suriya@gmail.com>
2024-03-05 22:34:25 +0100

    add swiper basic HTML

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -1,5 +1,4 @@
 import { Controller } from "@hotwired/stimulus"
-// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 import Hammer from "hammerjs";
 
 export default class extends Controller {
@@ -8,33 +7,8 @@ export default class extends Controller {
   static values = {
     vocab: Number
   }
-  connect() {
-    // SWIPER.js
-    // this.swiper = new Swiper(this.element, {
-    //   effect: 'cards',
-    //   on: {
-    //     slideChange: () => {
-    //       const direction = this.swiper.activeIndex > this.swiper.previousIndex ? 'right' : 'left';
-    //       const studied = direction === 'left';
-    //       const vocabId = this.swiper.slides[this.swiper.activeIndex].dataset.vocabId
-    //       if (vocabId) {
-    //         fetch(`/flashcard_words/${vocabId}`, {
-    //           method: 'PATCH',
-    //           headers: {
-    //             'Content-Type': 'application/json',
-    //             'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-    //           },
-    //             body: JSON.stringify({ studied: studied })
-    //         })
-    //         // .then(
-    //         //   console.log(this.swiper.slides[this.swiper.activeIndex].firstElementChild)
-    //         // )
-    //       }
-    //     },
-    //   },
-    // });
 
-    // Hammer.js
-    console.log(this.vocabValue);
+  connect() {
+
   }
 }
